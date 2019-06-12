7afbc29e8a5cff368db65d8fb271246abc7cf3de
nat121234 <51712319+nat121234@users.noreply.github.com>
2019-06-12 10:00:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
-
+   # json_data = {'Hello': 'World!'}
+   # return jsonify(json_data)
+     return "น.ส.วิลาวัณย์ แววนกยูง เลขที่ 15 ชั้น ม.4/1"
 
 if __name__ == '__main__':
     app.run()
